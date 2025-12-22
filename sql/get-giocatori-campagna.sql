3d1cee914c7e5ddab628fc154f7703a899ab6b07
Kram037 <defenzomarco@gmail.com>
2025-12-22 21:46:00 +0100

    Fix: get_giocatori_campagna include anche il creatore se non Ã¨ DM e non ha invito accettato

--- sql/get-giocatori-campagna.sql
+++ sql/get-giocatori-campagna.sql
@@ -14,9 +14,10 @@ SET search_path = public
 AS $$
 DECLARE
     v_id_dm VARCHAR(10);
+    v_user_id VARCHAR(10);
 BEGIN
-    -- Ottieni l'ID del DM corrente
-    SELECT id_dm INTO v_id_dm
+    -- Ottieni l'ID del DM corrente e del creatore
+    SELECT id_dm, user_id INTO v_id_dm, v_user_id
     FROM campagne
     WHERE id = campagna_id_param;
     
@@ -29,7 +30,26 @@ BEGIN
     INNER JOIN inviti_campagna ic ON u.id = ic.invitato_id
     WHERE ic.campagna_id = campagna_id_param
     AND ic.stato = 'accepted'
-    AND u.id != v_id_dm; -- Escludi il DM corrente
+    AND u.id != COALESCE(v_id_dm, '') -- Escludi il DM corrente
+    
+    UNION
+    
+    -- Include anche il creatore se non è il DM e non è già nella lista degli inviti
+    SELECT 
+        u.id,
+        u.nome_utente,
+        u.cid
+    FROM utenti u
+    WHERE u.id = v_user_id
+    AND v_user_id IS NOT NULL
+    AND (v_user_id != COALESCE(v_id_dm, ''))
+    AND NOT EXISTS (
+        SELECT 1 
+        FROM inviti_campagna ic2 
+        WHERE ic2.invitato_id = u.id 
+        AND ic2.campagna_id = campagna_id_param 
+        AND ic2.stato = 'accepted'
+    );
 END;
 $$;
 
