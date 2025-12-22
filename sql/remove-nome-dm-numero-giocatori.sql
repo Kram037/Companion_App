3eba9b5134daafe02d8d18524f2b14efbe6e9d54
Kram037 <defenzomarco@gmail.com>
2025-12-22 23:35:57 +0100

    Fix: aggiunto rimozione trigger update_numero_giocatori_trigger prima di rimuovere il campo

--- sql/remove-nome-dm-numero-giocatori.sql
+++ sql/remove-nome-dm-numero-giocatori.sql
@@ -3,7 +3,14 @@
 -- - nome_dm: recuperato dalla tabella utenti tramite id_dm
 -- - numero_giocatori: calcolato contando gli elementi nell'array giocatori
 
--- Rimuovi i campi
+-- PRIMA: Rimuovi i trigger e le funzioni che usano numero_giocatori
+DROP TRIGGER IF EXISTS update_numero_giocatori_trigger ON campagne CASCADE;
+DROP FUNCTION IF EXISTS update_numero_giocatori_from_array() CASCADE;
+
+-- Nota: Il trigger sync_campagna_giocatori() viene mantenuto perché aggiorna solo l'array giocatori
+-- Se esiste un trigger che aggiorna anche numero_giocatori, rimuovilo qui
+
+-- POI: Rimuovi i campi
 ALTER TABLE campagne DROP COLUMN IF EXISTS nome_dm;
 ALTER TABLE campagne DROP COLUMN IF EXISTS numero_giocatori;
 
