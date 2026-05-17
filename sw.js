eb01713be2ff52b366830c9bb2d3b82f88818be0
Kram037 <defenzomarco@gmail.com>
2026-05-17 13:18:06 +0200

    Extract character sheet equipment module

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'companion-app-v30';
+const CACHE_NAME = 'companion-app-v31';
 
 const APP_SHELL_URLS = [
     './',
@@ -43,6 +43,7 @@ const APP_SHELL_URLS = [
     './js/Personaggi/personaggi-scheda-calcolatori.js',
     './js/Personaggi/personaggi-scheda-levelup-risorse.js',
     './js/Personaggi/personaggi-scheda-bonus.js',
+    './js/Personaggi/personaggi-scheda-equipaggiamento.js',
     './js/Personaggi/personaggi.js',
     './js/Sessioni/sessions.js',
     './js/Combattimento/combat.js',
