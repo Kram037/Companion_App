1952e93db153f3de9042cd035fe8e26f825d5f71
Kram037 <defenzomarco@gmail.com>
2026-05-17 13:16:07 +0200

    Extract character sheet calculators module

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'companion-app-v27';
+const CACHE_NAME = 'companion-app-v28';
 
 const APP_SHELL_URLS = [
     './',
@@ -40,6 +40,7 @@ const APP_SHELL_URLS = [
     './js/Personaggi/personaggi-scheda-inventario.js',
     './js/Personaggi/personaggi-scheda-privilegi.js',
     './js/Personaggi/personaggi-scheda-editor.js',
+    './js/Personaggi/personaggi-scheda-calcolatori.js',
     './js/Personaggi/personaggi.js',
     './js/Sessioni/sessions.js',
     './js/Combattimento/combat.js',
