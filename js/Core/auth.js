445d3950c5f51d6d08b27ce04db9f4386ad2afe2
Kram037 <defenzomarco@gmail.com>
2026-05-17 13:42:40 +0200

    Allow compendium during auth restore

--- js/Core/auth.js
+++ js/Core/auth.js
@@ -560,7 +560,7 @@ async function checkAuthState() {
                 renderSessioneContent(AppState.currentCampagnaId);
             } else if (AppState.currentPage === 'dettagli' && AppState.currentCampagnaId) {
                 navigateToPage('dettagli');
-            } else if (AppState.currentCampagnaId && !['campagne','amici','personaggi','laboratorio','scheda'].includes(AppState.currentPage)) {
+            } else if (AppState.currentCampagnaId && !['campagne','amici','compendio','personaggi','laboratorio','scheda'].includes(AppState.currentPage)) {
                 navigateToPage('dettagli');
             } else {
                 navigateToPage(AppState.currentPage || 'campagne');
